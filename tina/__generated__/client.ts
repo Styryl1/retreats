95bab8907e19ef91e78222b90df0489e4802bc71
Jordan <jordan.catterall@gmail.com>
2026-03-16 17:05:36 +0100

    Rebrand site to Soulfull experience

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/home/styryl/dev/retreats/tina/__generated__/.cache/1773259266923', url: 'http://localhost:4001/graphql', token: 'null', queries,  });
+export const client = createClient({ cacheDir: '/home/styryl/dev/retreats/tina/__generated__/.cache/1773676976218', url: 'http://localhost:4001/graphql', token: '7dc5ce0c1cc07d8ecce1bf68cc9fb2f1da8e8266', queries,  });
 export default client;
   
